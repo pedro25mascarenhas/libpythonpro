3ebcb84704a19dd223a40b16cd0a5115c261dadf
pedro <pedro25mascarenhas@gmail.com>
2023-06-28 20:13:17 -0300

    Corrigido o erro no setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpython"
+PACKAGE = "libpythonpro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Pedro Mascarenhas"

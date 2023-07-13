426b1cd2b4d8c931dace235f9c7d9e490aa4126f
pedro <pedro25mascarenhas@gmail.com>
2023-07-13 18:16:17 -0300

    Finalização dos Testes Automáticos

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,7 +1,9 @@
 class Enviador:
+
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente inválido: {remetente}')
+        self.qtd_email_enviados += 1
         return remetente
 
 
--- libpythonpro/tests/test_spam/teste_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/teste_envio_para_base_de_usuarios.py
@@ -1,11 +1,50 @@
+from unittest.mock import Mock
+
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Pedro', email='pedro25mascarenhas@gmail.com'),
+            Usuario(nome='Luciano', email='pedro25mascarenhas@gmail.com')
+        ],
+        [
+            Usuario(nome='Pedro', email='pedro25mascarenhas@gmail.com')
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'pedro25mascarenhas@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
-        )
+    )
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Pedro', email='pedro25mascarenhas@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'luciano25mascarenhas@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    enviador.enviar.assert_called_once_with(
+        'luciano25mascarenhas@gmail.com',
+        'pedro25mascarenhas@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
